d52cb172bc9709f4c13a97937e44cec77e5df302
LEONARDO CAVAZZANI <leonardo.cavazzani@skipthedishes.ca>
2020-06-05 21:39:35 -0500

    types

--- src/server.ts
+++ src/server.ts
@@ -12,49 +12,31 @@ import { Request, Response } from 'express';
   const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
+  app.use(bodyParser.json());  
   // Root Endpoint
-
-app.get('/filteredimage',  function(req, res) {
-
-  // Access the provided 'page' and 'limt' query parameters
-  let { image_url } = req.query;
-
-  const filteredImage = filterImageFromURL(image_url)
-  .then((filteredpath) => {
-    const filteredImages = [filteredpath]
-    res.status(201).send(filteredpath);
-    deleteLocalFiles(filteredImages)
-  })
-  .catch((e) => {
-    res.send(422).send("Error filtering image");
-  });
-  // Return the articles to the rendering engine
-});
-
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
+
+  app.get('/filteredimage',  function(req: Request, res: Response) {
+
+    // Access the provided 'page' and 'limt' query parameters
+    let { image_url } = req.query;
+
+    const filteredImage = filterImageFromURL(image_url)
+    .then((filteredpath:string) => {
+
+      const filteredImages:Array<string> = [filteredpath]
+      
+      res.status(201).send(filteredpath);
+      deleteLocalFiles(filteredImages)
+    })
+    .catch((e) => {
+      res.send(422).send("Error filtering image");
+    });
+    // Return the articles to the rendering engine
+  });
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
